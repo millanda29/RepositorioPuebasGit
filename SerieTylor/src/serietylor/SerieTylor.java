93895f2067f0956252c13793e27c0d938f91b6a0
millanda29 <maikolllanda@gmail.com>
2023-07-30 10:52:41 -0500

    We create the methods and request the data to enter in the main method

--- SerieTylor/src/serietylor/SerieTylor.java
+++ SerieTylor/src/serietylor/SerieTylor.java
@@ -4,6 +4,8 @@
  */
 package serietylor;
 
+import java.util.Scanner;
+
 /**
  *
  * @author maikd
@@ -14,7 +16,21 @@ public class SerieTylor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MetodoClass met = new MetodoClass(); 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa la función (por ejemplo, 'x^2 + 3*x + 5'): ");
+        String function = scanner.nextLine();
+
+        System.out.print("Ingresa el valor de x para calcular la serie de Taylor: ");
+        double x = scanner.nextDouble();
+
+        System.out.print("Ingresa el número de términos de la serie de Taylor: ");
+        int n = scanner.nextInt();
+
+        double result = met.calculateCustomTaylorSeries(function, x, n);
+
+        System.out.println("Resultado de la serie de Taylor: " + result);
     }
     
 }
