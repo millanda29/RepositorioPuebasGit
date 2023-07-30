70a93a096d76085340a449c8e747984489d51946
millanda29 <maikolllanda@gmail.com>
2023-07-30 14:28:19 -0500

    xD
    arreglo de class.java

--- Conversiones/src/conversiones/Conversiones.java
+++ Conversiones/src/conversiones/Conversiones.java
@@ -12,25 +12,8 @@ public class Conversiones {
 
     public static void main(String[] args) {
         main conv = new main();
-        int decimalNumber = 42;
-        String binaryString = "101010";
-        String hexadecimalString = "2A";
-        String octalString = "52";
-
-        String conversionType = "hexadecimal"; // Cambiar a "binary", "hexadecimal" u "octal" para probar otras conversiones
-
-        if (conversionType.equals("binary")) {
-            int decimalResult = conv.binaryToDecimal(binaryString);
-            System.out.println("El número binario " + binaryString + " en decimal es: " + decimalResult);
-        } else if (conversionType.equals("hexadecimal")) {
-            int decimalResult = conv.hexadecimalToDecimal(hexadecimalString);
-            System.out.println("El número hexadecimal " + hexadecimalString + " en decimal es: " + decimalResult);
-        } else if (conversionType.equals("octal")) {
-            int decimalResult = conv.octalToDecimal(octalString);
-            System.out.println("El número octal " + octalString + " en decimal es: " + decimalResult);
-        } else {
-            System.out.println("Opción de conversión no válida.");
-        }
+        
+        
     }
 
 }
