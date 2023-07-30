2675d5eda47b1e08f09905d8201adab585709b72
millanda29 <maikolllanda@gmail.com>
2023-07-30 14:50:25 -0500

    Update
    Actualizamos el main de Conversiones

--- Conversiones/src/conversiones/Conversiones.java
+++ Conversiones/src/conversiones/Conversiones.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package conversiones;
+import java.util.Scanner;
 
 /**
  *
@@ -12,7 +13,28 @@ public class Conversiones {
 
     public static void main(String[] args) {
         main conv = new main();
+        Scanner leer = new Scanner(System.in);
+        String d ="decimal", b ="binario", h ="hexadecimal", o ="octal";
+        String tipo1, tipo2;
+        String numero1;
+        int numero;
+        System.out.println("Ingrese el numero que desea ingresar");
+        numero1 = leer.next();
+        System.out.println("Ingrese el tipo de numero(decimal, binario, hexadecimal, octal)");
+        tipo1=leer.next();
+        System.out.println("Ingrese a que desea convertir (decimal, binario, hexadecimal, octal)");
+        tipo2=leer.next();
         
+        if ( tipo1.equalsIgnoreCase(d)) {
+            if ( tipo2.equalsIgnoreCase(b)) {
+                int number = Integer.parseInt(numero1);
+                conv.decimalToBinary(number);
+            } 
+            
+        } else {
+            
+        }
+        leer.close();
         
     }
 
